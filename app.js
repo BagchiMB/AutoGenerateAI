d53e1fb6d3115b0d7809d9b8c8916b53a898c962
unknown <mbagchi6884@gmail.com>
2019-11-17 04:14:23 +0530

    Minor desktop improvement final desktop version-2

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var scroll = new SmoothScroll('.navbar a[href*="#"]',{
     clip:true
     // offset:2
 });
+var scroll = new SmoothScroll('.contentDis a[href*="#"]',{
+  speed:1000,
+  clip:true
+  // offset:2
+});
 function openSlideMenu(){
     document.getElementById('side-menu').style.width = '250px';
     document.getElementById('main').style.marginLeft = '250px';
