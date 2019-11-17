4755e323f611884cffb5f6b666bee56869196665
unknown <mbagchi6884@gmail.com>
2019-11-18 00:20:40 +0530

    Api add ho gye

--- app.js
+++ app.js
@@ -18,34 +18,34 @@ function openSlideMenu(){
     document.getElementById('main').style.marginLeft = '0';
   }
 
-  var sections = $('section')
-  , nav = $('nav')
-  , nav_height = nav.outerHeight();
+//   var sections = $('section')
+//   , nav = $('nav')
+//   , nav_height = nav.outerHeight();
 
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
+// $(window).on('scroll', function () {
+//   var cur_pos = $(this).scrollTop();
   
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
+//   sections.each(function() {
+//     var top = $(this).offset().top - nav_height,
+//         bottom = top + $(this).outerHeight();
     
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('active');
-      sections.removeClass('active');
+//     if (cur_pos >= top && cur_pos <= bottom) {
+//       nav.find('a').removeClass('active');
+//       sections.removeClass('active');
       
-      $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-    }
-  });
-});
+//       $(this).addClass('active');
+//       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+//     }
+//   });
+// });
 
-nav.find('a').on('click', function () {
-  var $el = $(this)
-    , id = $el.attr('href');
+// nav.find('a').on('click', function () {
+//   var $el = $(this)
+//     , id = $el.attr('href');
   
-  $('html, body').animate({
-    scrollTop: $(id).offset().top - nav_height
-  }, 500);
+//   $('html, body').animate({
+//     scrollTop: $(id).offset().top - nav_height
+//   }, 500);
   
-  return false;
-});
+//   return false;
+// });
---
+++ handler.js
@@ -0,0 +1,29 @@
+
+    function sendData() {
+        console.log("test");
+        var form = document.getElementById("my_form");
+      var XHR = new XMLHttpRequest();
+  
+      // Bind the FormData object and the form element
+      var FD = new FormData(form);
+  
+      // Define what happens on successful data submission
+      XHR.addEventListener("load", function(event) {
+        document.getElementById('my_form').style.display = "none";
+
+        // document.getElementById('res').innerHTML = FD.get("name");
+        // document.getElementById('thankyou_message').style.display = "block";
+      });
+  
+      // Define what happens in case of error
+      XHR.addEventListener("error", function(event) {
+        alert('Oops! Something went wrong.');
+      });
+  
+      // Set up our request
+      XHR.open("POST", "http://34.93.49.60:8888/emailservice/api/");
+  
+      // The data sent is what the user provided in the form
+      XHR.send(FD);
+    }
+   
